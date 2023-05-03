d15a7a6f36f0d97be4baadba030128ffa0934d60
Ansel Bobrow <ansel.bobrow@gmail.com>
2023-05-02 23:19:54 -0400

    sucessfully demux pots and read patches

--- big_knob.cpp
+++ big_knob.cpp
@@ -2,6 +2,7 @@
 #include "daisysp.h"
 
 using namespace daisy;
+using namespace daisy::seed;
 using namespace daisysp;
 
 DaisySeed hw;
@@ -20,9 +21,37 @@ int main(void) {
     hw.SetAudioBlockSize(4);  // number of samples handled per callback
     hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
     hw.StartAudio(AudioCallback);
-    hw.StartLog(true);
-    hw.PrintLine("Hello World");
+
+    // adc setup
+    AdcChannelConfig adc_config;
+    adc_config.InitMux(A0, 8, D10, D11, D12);
+    hw.adc.Init(&adc_config, 1);
+    hw.adc.Start();
+
+    // GPIO setup
+    GPIO outWire;
+    GPIO inWire;
+    inWire.Init(D13, GPIO::Mode::INPUT, GPIO::Pull::PULLDOWN);
+    outWire.Init(D14, GPIO::Mode::OUTPUT, GPIO::Pull::NOPULL);
+    outWire.Write(1);
+
+    hw.StartLog();
+
+    int idx = 0;
 
     while (1) {
+        for (; idx < 4; idx++) {
+            float value = hw.adc.GetMuxFloat(0, idx);
+            hw.PrintLine("%d: %f", idx, value);
+        }
+        idx = 0;
+
+        if (inWire.Read()) {
+            hw.PrintLine("yes");
+        } else {
+            hw.PrintLine("no");
+        }
+
+        System::Delay(3000);
     }
 }
---
+++ patchbay.h
@@ -0,0 +1,4 @@
+#ifndef PATCHBAY_H
+#define PATCHBAY_H
+
+#endif  // #ifndef PATCHBAY_H
